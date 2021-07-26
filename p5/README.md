95429cb7ac94de1815848ef8eb65a3a2e93d589c
Sciencethebird <sciencethebird@gmail.com>
2021-07-26 18:34:09 +0800

    add p5

---
+++ p5/README.md
@@ -0,0 +1,8 @@
+Problem 5 Problem with Float Values
+===
+
+Note
+---
+
+Output
+---
---
+++ p5/p5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  float a = 1.134;
+  float b = 3.402;
+  if (a * 3 == b) {
+    printf("yes");
+  } else {
+    printf("no");
+  }
+}
