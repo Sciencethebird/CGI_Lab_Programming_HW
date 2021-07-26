c36bafc7743a31767fa2151e38d425d8da892d7f
Sciencethebird <sciencethebird@gmail.com>
2021-07-26 20:43:55 +0800

    complete p5

--- p5/README.md
+++ p5/README.md
@@ -3,6 +3,11 @@ Problem 5 Problem with Float Values
 
 Note
 ---
+- multiplication and eqaulibirum of float point
 
 Output
 ---
+```shell
+yes 
+# 1.132 *3 == 3.402
+```
--- p5/p5.c
+++ p5/p5.c
@@ -2,9 +2,12 @@
 int main() {
   float a = 1.134;
   float b = 3.402;
+  printf("%lf\n", 1.134*3);
   if (a * 3 == b) {
     printf("yes");
   } else {
     printf("no");
   }
+  return 0;
 }
+
