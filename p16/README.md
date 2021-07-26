17e3cd81400d99accb3c0d0e54a95bdc331f4550
Sciencethebird <sciencethebird@gmail.com>
2021-07-27 02:16:43 +0800

    complete p16

---
+++ p16/README.md
@@ -0,0 +1,13 @@
+Problem Unsigned int 
+===
+
+Output Explained
+---
+- when compare with negative value, the negative value is convert into unsigned int
+
+Output
+---
+```sh
+ui + 1 > 0
+ui + 1 < -1
+```
---
+++ p16/p16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  unsigned int ui = 2147483647; //4294967295
+
+  unsigned int neg = -2;
+  printf("%u\n", neg);
+
+  if (ui + 1 > 0)
+    printf("ui + 1 > 0\n"); 
+  if (ui + 1 < -1)
+    printf("ui + 1 < -1\n");
+}
