92fb44669438aa17fa50018bd6d331ccfa2b9b1b
Sciencethebird <sciencethebird@gmail.com>
2021-07-28 11:58:55 +0800

    update p9

--- p09/p9.c
+++ p09/p9.c
@@ -3,7 +3,7 @@ int main() {
   FILE *fp = fopen("file", "wb");
   char temp = 255;
   printf("%d\n", temp+1);
-  for (char c = 0; c < 250; c++) {
+  for (char c = 0; c < 256; c++) {
     printf("%d\n", c);
     //fputc(c, fp);
   }
